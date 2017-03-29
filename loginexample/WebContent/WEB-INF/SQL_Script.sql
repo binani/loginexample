81b94c4eb73e68f913dabbfe0771d31bf0e358af
Madhu Babu <Madhu Babu@LAPTOP-M18AEVC3>
2017-03-29 15:47:59 -0400

    Database script file attached.

---
+++ loginexample/WebContent/WEB-INF/SQL_Script.sql
@@ -0,0 +1,11 @@
+CREATE TABLE venderinfo ( 
+
+VendorName tinytext NOT NULL, 
+
+VendorPass varchar(50) NOT NULL, 
+
+VendorRepass varchar(50) NOT NULL 
+
+);
+
+insert into venderinfo values ('admin', 'admin123', 'admin123')
